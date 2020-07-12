ff87bffcfb12cb19e7c668c8f617b6750e269efd
Sahana Srinivasan <sahanas009@gmail.com>
2020-07-11 17:07:45 -0700

    add financial add analysis

--- sahana-fall2020-survey-analysis.R
+++ sahana-fall2020-survey-analysis.R
@@ -71,7 +71,6 @@ ggplot(data=subset(df, (!is.na(location_off_campus) & location_off_campus != "")
   scale_x_discrete(labels = function(x) str_wrap(x, width = 20), 
                    limits = c("Family Home", "Personal Apartment/Home (not with Harvard friends)", "Living with Harvard friends", "Other")) +
   geom_text(stat='count', aes(label=percent((..count..)/sum((..count..)))), vjust=-1) +
-  ylim(c(0, 450)) + 
   xlab("Location") + 
   ylab("Count") + 
   labs(title="Where would off-campus students live?") +
@@ -87,7 +86,6 @@ ggplot(data=subset(df, (!is.na(opinion_eligibility) & opinion_eligibility != "")
   scale_x_discrete(labels = function(x) str_wrap(x, width = 20), 
                    limits = c("Allows too few students back on campus", "Just right", "Number is just right, but it should be a different cohort in the Fall", "Allows too many students back on campus", "Don't know enough to say")) +
   geom_text(stat='count', aes(label=percent((..count..)/sum((..count..)))), vjust=-1) +
-  ylim(c(0, 700)) + 
   xlab("Opinion") + 
   ylab("Count") + 
   labs(title="Opinion on Number of Students Allowed Back") +
@@ -138,10 +136,9 @@ grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = 
 # university's plans do you prefer
 ggplot(data=subset(df, (!is.na(other_school) & other_school != "Other (please specify)" & other_school != "Don't know enough to say" & other_school != "")), aes(x=factor(other_school))) + 
   geom_bar(aes(fill = factor(other_school))) + 
-  scale_fill_manual(values = c(primary[1], primary[2], primary[4], primary[6],primary[3], "#D84742", primary[5], "#760000")) + 
+  scale_fill_manual(values = c("#B31B1B", "#00693e", "#012169", "#a31f34", "#D41B2C", "#e77500", "#C41E3A", "#00356B")) + 
   scale_x_discrete(labels = function(x) str_wrap(x, width = 20)) +
   geom_text(stat='count', aes(label=percent((..count..)/sum((..count..)))), vjust=-1) +
-  ylim(c(0, 250)) + 
   xlab("University") + 
   ylab("Count") + 
   labs(title="Preferred University Fall 2020 Plan") +
@@ -150,6 +147,66 @@ ggplot(data=subset(df, (!is.na(other_school) & other_school != "Other (please sp
 grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = unit(1.5, 'cm'))
 
 # sentiment analysis of free text
+
+# breaking down opinion by international students
+
+# breaking down opinions by financial aid
+
+# financial aid question support_financial_ai
+nrow(df)
+finaidf <- df[!is.na(df$support_financial_ai) & df$support_financial_ai != "", ]; nrow(finaidf)
+notenough <- nrow(finaidf[grepl("Not enough money provided to off-campus students", finaidf$support_financial_ai, fixed = TRUE), ])
+individ <- nrow(finaidf[grepl("Policy is not individualized enough", finaidf$support_financial_ai, fixed = TRUE),])
+agree <- nrow(finaidf[grepl("Agree with the policy", finaidf$support_financial_ai),])
+nrow(finaidf); notenough; individ; agree
+notenough
+
+finaid_op <- c(notenough, individ, agree)
+finaid_op
+
+finaid_ops <- c("Not enough money provided to off-campus students", "Policy is not individualized enough", "Agree with the policy")
+finaid_data = data.frame(finaid_ops, finaid_op)
+finaid_data
+ggplot(data=finaid_data, aes(x=finaid_ops, y = finaid_op)) + 
+  geom_bar(stat = 'identity', aes(fill = finaid_ops)) + 
+  scale_fill_manual(values = c(primary[3], primary[1], primary[2])) + 
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20), 
+                   limits = finaid_ops) +
+  geom_text(aes(label=percent(finaid_op / nrow(finaidf))), vjust = -1.5) +
+  xlab("Opinion") + 
+  ylab("Count") + 
+  labs(title="Financial Aid Policy Opinions") +
+  theme_hodp() +
+  theme(legend.position = "none")
+grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = unit(1.5, 'cm'))
+
+# finaid opinions among those on finaid
+nrow(df)
+fdf <- df[!is.na(df$support_financial_ai) & df$support_financial_ai != "" & df$financial_aid == "Yes", ]; nrow(fdf)
+notenough <- nrow(fdf[grepl("Not enough money provided to off-campus students", fdf$support_financial_ai, fixed = TRUE), ])
+individ <- nrow(fdf[grepl("Policy is not individualized enough", fdf$support_financial_ai, fixed = TRUE),])
+agree <- nrow(fdf[grepl("Agree with the policy", fdf$support_financial_ai),])
+nrow(fdf); notenough; individ; agree
+notenough
+
+finaid_op <- c(notenough, individ, agree)
+finaid_op
+
+finaid_ops <- c("Not enough money provided to off-campus students", "Policy is not individualized enough", "Agree with the policy")
+finaid_data = data.frame(finaid_ops, finaid_op)
+finaid_data
+ggplot(data=finaid_data, aes(x=finaid_ops, y = finaid_op)) + 
+  geom_bar(stat = 'identity', aes(fill = finaid_ops)) + 
+  scale_fill_manual(values = c(primary[3], primary[1], primary[2])) + 
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 20), 
+                   limits = finaid_ops) +
+  geom_text(aes(label=percent(finaid_op / nrow(fdf))), vjust = -1.5) +
+  xlab("Opinion") + 
+  ylab("Count") + 
+  labs(title="Financial Aid Policy Opinions") +
+  theme_hodp() +
+  theme(legend.position = "none")
+grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = unit(1.5, 'cm'))
 
 # most frequent words and word cloud of free text
 
